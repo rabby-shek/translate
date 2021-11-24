8457f0272ff429942b10ec1a902144de8a177348
rabby-shek <rabbyshek1998@gmail.com>
2021-11-25 00:17:30 +0600

    last two section added

---
+++ english/Office_Time_Setting_Form.php
@@ -0,0 +1,15 @@
+<?php 
+		return[
+			'Office Time Setting Form',
+			'Start Date',
+			'Employee Name',
+			'End Date',
+			'In Time',
+			'Out Time',
+			'date',
+			'duty time',
+			'created at',
+			
+
+
+		];
---
+++ english/User_Registration_Form.php
@@ -0,0 +1,16 @@
+<?php 
+		return[
+			'User Registration Form',
+			'Employee Information',
+			'Employee Name',
+			'Designation',
+			'Phone',
+			'Address',
+			'Authentication Information',
+			'Role',
+			'Email',
+			'Password',
+			'Confirm Password',
+
+
+		];
